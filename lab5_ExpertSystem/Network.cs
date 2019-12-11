e8d3152d174938a928ec6eefa2b4ce1bd83fdf14
leko13st <lekomstas13@mail.ru>
2019-12-12 00:46:57 +0500

    added auto-creating of layers

--- lab5_ExpertSystem/Network.cs
+++ lab5_ExpertSystem/Network.cs
@@ -20,7 +20,47 @@ namespace lab5_ExpertSystem
             Createoutput();
         }
 
-       
+        public void Createinput()
+        {
+            var neirons = new List<Neiron>();
+            for (int i = 0; i < Opisanie.inputCount; i++)
+            {
+                var neuron = new Neiron(1, 1);
+                neirons.Add(neuron);
+            }
+            var sloi = new Sloi(neirons, 1);
+            slois.Add(sloi);
+        }
+
+        public void Createoutput()
+        {
+            var neirons = new List<Neiron>();
+            var lastSloi = slois.Last();
+            for (int i = 0; i < Opisanie.outputCount; i++)
+            {
+                var neuron = new Neiron(lastSloi.Count, 3);
+                neuron.name = i.ToString();
+                neirons.Add(neuron);
+            }
+            var sloi = new Sloi(neirons, 3);
+            slois.Add(sloi);
+        }
+
+        public void CreateSloi()
+        {
+            for (int j = 0; j < Opisanie.sloiCount.Count; j++)
+            {
+                var neirons = new List<Neiron>();
+                var lastSloi = slois.Last();
+                for (int i = 0; i < Opisanie.sloiCount[j]; i++)
+                {
+                    var neuron = new Neiron(lastSloi.Count, 2);
+                    neirons.Add(neuron);
+                }
+                var sloi = new Sloi(neirons, 2);
+                slois.Add(sloi);
+            }
+        }
 
         public Neiron FeedForvard(params double[] inputSignals)
         {
